98633e7b78e94ab9f2fc583615e52cd928112471
Winston Tsia <winstontsia@gmail.com>
2019-11-19 02:16:00 -0800

    Updated settings

--- backend/settings.py
+++ backend/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rn0j-^byy1fk+4j#akc+zo1k9rehg*0xiqawl_%@t&2_us2$vt'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mybugappbackend.herokuapp.com']
 
 
 # Application definition
